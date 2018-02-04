44da9d94c991029dd654e90a818fa7da60e458d6
NathanYukai <zlcghoul@hotmail.com>
2018-02-04 15:09:39 +0000

    set-up

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: "./src/app.ts",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".js", ".json"],
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.ts$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+        ]
+    },
+    externals: {
+        phaser: "phaser-ce"
+    }
+
+};
